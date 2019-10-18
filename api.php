d59e0a586cf71dc9a700b1f913e7ca53a39ed68e
root <root@localhost.localdomain>
2019-10-18 20:51:27 +0200

    init

---
+++ api.php
@@ -0,0 +1,292 @@
+<?php
+
+error_reporting(~0);
+ini_set('display_errors', 1);
+
+$db_host = "localhost";
+$db_user = "test";
+$db_password = "test";
+$db_schema = "tools";
+
+
+header("Content-type: application/json");
+
+if (isset($_GET["ip"])) {
+    $host = gethostname();
+    $ip = gethostbyname($host);
+    $ext_ip = file_get_contents("http://ipecho.net/plain");
+
+    echo json_encode(array("host" => $host, "ip" => $ip, "external_ip" => $ext_ip));
+}
+
+if (isset($_GET["nav"])) {
+    $nav = array(
+        array("title" => "Tweetdeck", "link" => "https://tweetdeck.twitter.com/", "target" => "_blank"),
+        array("title" => "Whatsapp", "link" => "https://web.whatsapp.com/", "target" => "_blank"),
+        array("title" => "Gmail", "link" => "https://mail.google.com/mail/u/0/#inbox", "target" => "_blank")
+    );
+
+    echo json_encode($nav);
+}
+
+if (isset($_GET["todo"])) {
+    $db_link = mysqli_connect($db_host, $db_user, $db_password, $db_schema);
+
+    if (isset($_POST["id"], $_POST["text"])) {
+        $id = mysqli_real_escape_string($db_link, $_POST["id"]);
+        $text = utf8_decode(mysqli_real_escape_string($db_link, $_POST["text"]));
+        $address = mysqli_real_escape_string($db_link, $_SERVER['REMOTE_ADDR']);
+
+        if (!empty($id) && !empty($text)) {
+            $sql = "UPDATE `todo` SET `text` = '$text', updated = now(), updatedby = '$address' WHERE `todo`.`id` = " . (int) $id . ";";
+            $db_erg = mysqli_query($db_link, $sql);
+
+            echo mysqli_error($db_link);
+        }
+    }
+
+    // Insert Todo
+    else if (isset($_POST["todo"])) {
+        $todo = utf8_decode(mysqli_real_escape_string($db_link, $_POST["todo"]));
+        $address = mysqli_real_escape_string($db_link, $_SERVER['REMOTE_ADDR']);
+
+        if (!empty($todo)) {
+            $sql = "INSERT INTO `todo` (`text`, `deleted`, `created`, `createdby`) VALUES('$todo', null, now(), '$address');";
+            $db_erg = mysqli_query($db_link, $sql);
+            
+            echo mysqli_error($db_link);
+        }
+    }
+
+    // Insert Todo
+    else if (isset($_POST["search"])) {
+        $search = utf8_decode(mysqli_real_escape_string($db_link, $_POST["search"]));
+        $sql = 'select t.id, t.`text`, date_format(t.created, "%d.%m.%Y %H:%i:%s") as created, date_format(t.updated, "%d.%m.%Y %H:%i:%s") as updated , date_format(t.deleted, "%d.%m.%Y %H:%i:%s") as deleted, t.createdby, t.updatedby, t.deletedby from todo t';
+
+        if (is_numeric($search)) {
+            $sql .= ' where t.id =' . $search . ';';
+        } else {
+            $sql .= ' where t.text like("%' . $search . '%");';
+        }
+
+        $todos = loadSQL($sql);
+
+        $todos_with_links = genLinks($todos);
+
+        echo json_encode($todos_with_links);
+    }
+
+    // Delete or Restore Todo
+    else if (isset($_POST["id"])) {
+        $id = mysqli_real_escape_string($db_link, $_POST["id"]);
+        $address = mysqli_real_escape_string($db_link, $_SERVER['REMOTE_ADDR']);
+
+        if (!empty($id)) {
+            $sql = "select * from todo WHERE id = $id";
+            $db_erg = mysqli_query($db_link, $sql);
+            while ($zeile = mysqli_fetch_array($db_erg, MYSQLI_ASSOC)) {
+                $del = $zeile["deleted"];
+            }
+
+            if ($del == 'null' or $del == '0' or $del == 0) {
+                $sql = "UPDATE `todo` SET `deleted` = now(), deletedby = '$address' WHERE `todo`.`id` = $id;";
+                $db_erg = mysqli_query($db_link, $sql);
+
+                echo mysqli_error($db_link);
+            } else {
+                $sql = "UPDATE `todo` SET `deleted` = null, `updated` = now(), deletedby = null, updatedby = '$address' WHERE `todo`.`id` = $id;";
+                $db_erg = mysqli_query($db_link, $sql);
+
+                echo mysqli_error($db_link);
+            }
+        }
+    }
+
+
+    // Load Todos
+    if (isset($_GET["todos"])) {
+        $todos = loadSQL('select t.id, t.`text`, date_format(t.created, "%d.%m.%Y %H:%i:%s") as created, date_format(t.updated, "%d.%m.%Y %H:%i:%s") as updated , date_format(t.deleted, "%d.%m.%Y %H:%i:%s") as deleted, t.createdby, t.updatedby, t.deletedby from todo t where t.deleted is null or ( MONTH(t.deleted) = MONTH(now()) and YEAR(t.deleted) = YEAR(now())) order by t.id desc');
+
+        $todos_with_links = genLinks($todos);
+
+        echo json_encode($todos_with_links);
+    }
+
+
+    //Load Stats
+    if (isset($_GET["stats"])) {
+        echo json_encode(stats());
+    }
+
+    mysqli_close($db_link);
+}
+
+function loadSQL($sql) {
+    global $db_link;
+    $result = mysqli_query($db_link, $sql);
+    $todos = array();
+    if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $row_data = array();
+            foreach ($row as $k => $v) {
+                $row_data += array($k => (utf8_encode($v)));
+            }
+            $todos[] = $row_data;
+        }
+        $result->close();
+    }
+    return $todos;
+}
+
+function loadTodosCount() {
+    global $db_link;
+    $sql = 'select count(t.id) as amount from todo t';
+    $result = mysqli_query($db_link, $sql);
+    $amount = 0;
+    if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $amount = $row['amount'];
+        }
+        $result->close();
+    }
+    return $amount;
+}
+
+function stats() {
+    $open_now = 0;
+    $open_this_day = 0;
+    $closed_this_day = 0;
+    $open_this_week = 0;
+    $closed_this_week = 0;
+    $open_this_month = 0;
+    $closed_this_month = 0;
+    $all_todos = loadTodosCount();
+
+
+    $week = grk_Week_Range(date("Y-m-d h:i:sa"), -1);
+    $month = grk_Month_Range(date("Y-m-d h:i:sa"), -1);
+
+
+    foreach (loadSQL('select t.id, t.deleted, t.created, t.updated, t.createdby, t.updatedby, t.deletedby
+from todo t 
+where t.deleted is null or ( MONTH(t.deleted) = MONTH(now()) and YEAR(t.deleted) = YEAR(now()))') as $k => $v) {
+        $datetime1 = new DateTime($v['created']);
+        $datetime3 = new DateTime('today');
+
+        $datetime1->setTime(0, 0, 0);
+        $datetime3->setTime(0, 0, 0);
+
+        $i1 = date_diff($datetime1, $datetime3);
+
+        if ($v['deleted'] != null) {
+            $datetime2 = new DateTime($v['deleted']);
+
+            $datetime2->setTime(0, 0, 0);
+
+            $i2 = date_diff($datetime2, $datetime3);
+
+            if ($i2->format('%a') == 0) {
+                $closed_this_day++;
+            }
+            if ($datetime2->format('Y-m-d') <= $week[1] && $datetime2->format('Y-m-d') >= $week[0]) {
+                $closed_this_week++;
+            }
+            if ($datetime2->format('Y-m-d') <= $month[1] && $datetime2->format('Y-m-d') >= $month[0]) {
+                $closed_this_month++;
+            }
+        } else {
+            $open_now++;
+        }
+
+        if ($i1->format('%a') == 0) {
+            $open_this_day++;
+        }
+
+        if ($v['created'] < $week[1] && $v['created'] > $week[0]) {
+            $open_this_week++;
+        }
+
+        if ($v['created'] < $month[1] && $v['created'] > $month[0]) {
+            $open_this_month++;
+        }
+    }
+
+    return array(
+        array("title" => "Open: ", "amount" => $open_now, "label" => "label-danger"),
+        array("title" => "New Today: ", "amount" => $open_this_day, "label" => "label-warning"),
+        array("title" => "Done Today: ", "amount" => $closed_this_day, "label" => "label-success"),
+        array("title" => "New This Week: ", "amount" => $open_this_week, "label" => "label-warning"),
+        array("title" => "Done This Week: ", "amount" => $closed_this_week, "label" => "label-success"),
+        array("title" => "New This Month: ", "amount" => $open_this_month, "label" => "label-warning"),
+        array("title" => "Done This Month: ", "amount" => $closed_this_month, "label" => "label-success"),
+        array("title" => "All: ", "amount" => $all_todos, "label" => "label-info")
+    );
+}
+
+function grk_Week_Range($DateString, $FirstDay = 6) {
+    if (empty($DateString) === TRUE) {
+        $DateString = date('Y-m-d');
+    }
+    $Days = array(
+        0 => 'monday',
+        1 => 'tuesday',
+        2 => 'wednesday',
+        3 => 'thursday',
+        4 => 'friday',
+        5 => 'saturday',
+        6 => 'sunday'
+    );
+    $DT_Min = new DateTime('last ' . (isset($Days[$FirstDay]) === TRUE ? $Days[$FirstDay] : $Days[6]) . ' ' . $DateString);
+    $DT_Max = clone($DT_Min);
+    return array(
+        $DT_Min->modify('+1 day')->format('Y-m-d'),
+        $DT_Max->modify('+7 days')->format('Y-m-d')
+    );
+}
+
+function grk_Month_Range($DateString, $FirstDay = 1) {
+    if (empty($DateString) === TRUE) {
+        $DateString = date('Y-m-d');
+    }
+    $DT_Min = new DateTime('now');
+    $DT_Max = clone($DT_Min);
+    return array(
+        $DT_Min->format('Y-m-01'),
+        $DT_Max->format('Y-m-t')
+    );
+}
+
+function genLinks($array) {
+    $out = array();
+    foreach ($array as $k => $v) {
+
+        $links = array();
+
+        if (isset($v["text"])) {
+            $temp = explode(" ", str_replace("\r", " ", str_replace("\n", " ", $v["text"])));
+            if (is_array($temp)) {
+                foreach ($temp as $k2 => $v2) {
+                    if (startsWith($v2, "http")) {
+                        $short_temp = explode("//", $v2);
+
+                        if (isset($short_temp[1])) {
+                            $short = $short_temp[1];
+                            $links[] = array("link" => $v2, "short" => $short);
+                        }
+                    }
+                }
+            }
+        }
+        $v["links"] = $links;
+        $out[] = $v;
+    }
+
+    return $out;
+}
+
+function startsWith($string, $startString) {
+    $len = strlen($startString);
+}
+
+?>
+
---
+++ js/tools_app.js
@@ -0,0 +1,181 @@
+var app = angular.module("tools", []);
+
+app.controller("sideBar", function($scope, $http) {
+	$scope.load = function() {
+        $http({
+            method: 'GET',
+            url: 'api.php?ip'
+        }).then(function successCallback(response) {
+            $scope.ip = response.data;
+        });
+    }
+
+    $scope.load();
+});
+
+app.controller("todoList", function($scope, $http) {
+    $scope.load = function() {
+        $http({
+            method: 'GET',
+            url: 'api.php?todo&todos'
+        }).then(function successCallback(response) {
+            $scope.todos = response.data;
+        });
+        $http({
+            method: 'GET',
+            url: 'api.php?todo&stats'
+        }).then(function successCallback(response) {
+            $scope.stats = response.data;
+        });
+    }
+
+    $scope.load();
+    
+    $scope.refresh = function(todo) {
+        $scope.load();
+    }
+
+    $scope.save = function(todo) {
+        $http({
+            method: 'POST',
+            url: 'api.php?todo',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {
+                todo: todo
+            }
+        }).then(function successCallback(response) {
+            if (response.status != 200) {
+                alert(response.data);
+            }
+            $scope.todo = "";
+            $scope.load();
+        });
+    }
+
+    $scope.edit = function(todo) {
+        $http({
+            method: 'POST',
+            url: 'api.php?todo',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {
+                id: todo.id,
+                text: todo.text
+            }
+        }).then(function successCallback(response) {
+            if (response.status != 200) {
+                alert(response.data);
+            }
+            $scope.load();
+        });
+    }
+
+    $scope.search = function(todo) {
+        $http({
+            method: 'POST',
+            url: 'api.php?todo',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {
+                search: todo
+            }
+        }).then(function successCallback(response) {
+            $scope.todos = response.data;
+        });
+        $scope.todo = "";
+    }
+
+    $scope.delete = function(todo) {
+        $http({
+            method: 'POST',
+            url: 'api.php?todo',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {
+                id: todo.id
+            }
+        }).then(function successCallback(response) {
+            if (response.status != 200) {
+                alert(response.data);
+            }
+
+            $scope.load();
+        });
+    }
+
+    $scope.restore = function(todo) {
+        $http({
+            method: 'POST',
+            url: 'api.php?todo',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {
+                id: todo.id
+            }
+        }).then(function successCallback(response) {
+            if (response.status != 200) {
+                alert(response.data);
+            }
+
+            $scope.load();
+        });
+    }
+    
+    
+    
+    
+    $scope.copy = function(text) {
+        var dummy = document.createElement("textarea");
+        document.body.appendChild(dummy);
+        dummy.value = text;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+    }
+
+});
+
+app.controller("navigation", function($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'api.php?nav'
+    }).then(function successCallback(response) {
+        $scope.navigation = response.data;
+    });
+});
