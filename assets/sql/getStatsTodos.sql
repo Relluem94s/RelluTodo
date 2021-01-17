0b7e1dcac6698e7d2b0feee85159da06b85bf810
Relluem94 <github@relluem94.de>
2021-01-18 00:44:12 +0100

    fixed null on start of week month or year. also if there are no todos.

--- assets/sql/getStatsTodos.sql
+++ assets/sql/getStatsTodos.sql
@@ -3,41 +3,81 @@
  * Created: 19.12.2020
  */
 
-SELECT sum(CASE
-               WHEN t.deletedby IS NULL THEN 1
-           END) AS "open",
-       sum(CASE
-            WHEN Day(t.created) = Day(Now())
-            AND YEARWEEK(t.created, 1) = YEARWEEK(Now(), 1)
-            AND Month(t.created) = Month(Now())
-            AND Year(t.created) = Year(Now()) THEN 1
-           END) AS "createdDay",
-       sum(CASE
-               WHEN t.deletedby IS NOT NULL
+SELECT 
+    coalesce(
+        sum(
+            CASE 
+                WHEN t.deletedby IS NULL 
+                THEN 1
+            END
+        ),
+        0
+    ) AS "open",
+    coalesce(
+        sum(
+            CASE
+                WHEN Day(t.created) = Day(Now())
+                AND YEARWEEK(t.created, 1) = YEARWEEK(Now(), 1)
+                AND Month(t.created) = Month(Now())
+                AND Year(t.created) = Year(Now()) THEN 1
+            END
+        ),
+        0
+    ) AS "createdDay",
+    coalesce(
+        sum(
+            CASE
+                WHEN t.deletedby IS NOT NULL
                 AND Day(t.deleted) = Day(Now())
                 AND YEARWEEK(t.deleted, 1) = YEARWEEK(Now(), 1)
                 AND Month(t.deleted) = Month(Now())
                 AND Year(t.deleted) = Year(Now()) THEN 1
-           END) AS "deletedDay",
-       sum(CASE
-               WHEN YEARWEEK(t.created, 1) = YEARWEEK(Now(), 1)
+            END
+        ),
+        0
+    ) AS "deletedDay",
+    coalesce(
+        sum(
+            CASE
+                WHEN YEARWEEK(t.created, 1) = YEARWEEK(Now(), 1)
                 AND Month(t.created) = Month(Now())
                 AND Year(t.created) = Year(Now()) THEN 1
-           END) AS "createdWeek",
-       sum(CASE
-               WHEN t.deletedby IS NOT NULL
+            END
+        ),
+        0
+    ) AS "createdWeek",
+    coalesce(
+        sum(
+            CASE
+                WHEN t.deletedby IS NOT NULL
                 AND YEARWEEK(t.deleted, 1) = YEARWEEK(Now(), 1)
                 AND Month(t.deleted) = Month(Now())
                 AND Year(t.deleted) = Year(Now()) THEN 1
-           END) AS "deletedWeek",
-       sum(CASE
-               WHEN Month(t.created) = Month(Now())
+            END
+        ),
+        0
+    ) AS "deletedWeek",
+    coalesce(
+        sum(
+            CASE
+                WHEN Month(t.created) = Month(Now())
                 AND Year(t.created) = Year(Now()) THEN 1
-           END) AS "createdMonth",
-       sum(CASE
-               WHEN t.deletedby IS NOT NULL
+            END
+        ),
+        0
+    ) AS "createdMonth",
+    coalesce(
+        sum(
+            CASE
+                WHEN t.deletedby IS NOT NULL
                 AND Month(t.deleted) = Month(Now())
                 AND Year(t.deleted) = Year(Now()) THEN 1
-           END) AS "deletedMonth",
-       count(*) AS "total"
+            END
+        ),
+        0
+    ) AS "deletedMonth",
+    coalesce(
+        count(*),
+        0
+    ) AS "total"
 FROM todo t
