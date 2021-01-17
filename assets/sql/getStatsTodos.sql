cfd6db4a466304fe8a4a30414d52a4d65f07e9f2
Relluem94 <github@relluem94.de>
2021-01-17 17:52:30 +0100

    changed sql to see stats directly

--- assets/sql/getStatsTodos.sql
+++ assets/sql/getStatsTodos.sql
@@ -3,14 +3,41 @@
  * Created: 19.12.2020
  */
 
-SELECT t.id, 
-       t.deleted, 
-       t.created, 
-       t.updated, 
-       t.createdby, 
-       t.updatedby, 
-       t.deletedby 
-FROM   todo t 
-WHERE  t.deleted IS NULL 
-        OR ( Month(t.deleted) = Month(Now()) 
-             AND Year(t.deleted) = Year(Now()) ) 
+SELECT sum(CASE
+               WHEN t.deletedby IS NULL THEN 1
+           END) AS "open",
+       sum(CASE
+            WHEN Day(t.created) = Day(Now())
+            AND Week(t.created) = Week(Now())
+            AND Month(t.created) = Month(Now())
+            AND Year(t.created) = Year(Now()) THEN 1
+           END) AS "createdDay",
+       sum(CASE
+               WHEN t.deletedby IS NOT NULL
+                AND Day(t.deleted) = Day(Now())
+                AND Week(t.deleted) = Week(Now())
+                AND Month(t.deleted) = Month(Now())
+                AND Year(t.deleted) = Year(Now()) THEN 1
+           END) AS "deletedDay",
+       sum(CASE
+               WHEN Week(t.created) = Week(Now())
+                AND Month(t.created) = Month(Now())
+                AND Year(t.created) = Year(Now()) THEN 1
+           END) AS "createdWeek",
+       sum(CASE
+               WHEN t.deletedby IS NOT NULL
+                AND Week(t.deleted) = Week(Now())
+                AND Month(t.deleted) = Month(Now())
+                AND Year(t.deleted) = Year(Now()) THEN 1
+           END) AS "deletedWeek",
+       sum(CASE
+               WHEN Month(t.created) = Month(Now())
+                AND Year(t.created) = Year(Now()) THEN 1
+           END) AS "createdMonth",
+       sum(CASE
+               WHEN t.deletedby IS NOT NULL
+                AND Month(t.deleted) = Month(Now())
+                AND Year(t.deleted) = Year(Now()) THEN 1
+           END) AS "deletedMonth",
+       count(*) AS "total"
+FROM todo t
